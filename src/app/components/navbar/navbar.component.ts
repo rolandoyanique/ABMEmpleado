275f2164b5c1de4bf43b990a8f2ec99d28c682c7
Rolando <rolyan66@gmail.com>
2025-10-15 23:49:24 -0400

    Se realizo la alineacion del titulo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSliderModule} from '@angular/material/slider';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule
   ],
   exports:[
     MatSliderModule
